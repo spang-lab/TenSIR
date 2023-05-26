70186ce9996f30b1d1422d1d2f09015b1abbcf95
sueskind <52210599+sueskind@users.noreply.github.com>
2023-05-26 15:41:14 +0200

    Improve hparams_optimizer.py

--- source/hparams_optimizer.py
+++ source/hparams_optimizer.py
@@ -1,5 +1,7 @@
 import itertools
 import logging
+import math
+import sys
 import time
 from typing import Optional
 
@@ -15,6 +17,8 @@ SEED = 1
 THREADS = 32
 TARGET_ESS = 100
 Theta0 = (0.1, 0.1)
+prior_bounds = (0.01, 1.)
+burnin_frac = 0.1
 
 
 def ess_over_time(hparams: tuple, func: callable) -> Optional[float]:
@@ -24,7 +28,7 @@ def ess_over_time(hparams: tuple, func: callable) -> Optional[float]:
                                           end=f"2020-07-01",
                                           cache_path=AUSTRIA_DATA_CACHE_PATH)
 
-    iterator = func(data_sir, Theta0, *hparams, threads=THREADS)
+    iterator = func(data_sir, Theta0, prior_bounds, *hparams, threads=THREADS)
 
     # get one point to ensure initialization of all internal variables
     next(iterator)
@@ -36,15 +40,17 @@ def ess_over_time(hparams: tuple, func: callable) -> Optional[float]:
         alphas = np.append(alphas, alpha)
         betas = np.append(betas, beta)
 
-        ess_alpha = arviz.ess(alphas, method="folded")
-        ess_beta = arviz.ess(betas, method="folded")
+        burnin = math.ceil(len(alphas) * burnin_frac)
+
+        ess_alpha = arviz.ess(alphas[burnin:], method="folded")
+        ess_beta = arviz.ess(betas[burnin:], method="folded")
 
         logging.warning(f"ESS alpha: {ess_alpha}, ESS beta: {ess_beta}, wtime: {time.perf_counter() - start:.1f}s")
 
         if ess_alpha > TARGET_ESS and ess_beta > TARGET_ESS:
             break
 
-        if time.perf_counter() - start > 100:
+        if time.perf_counter() - start > 300:
             logging.critical(f"Aborting with ESS alpha: {ess_alpha}, ESS beta: {ess_beta}")
             return None
 
@@ -55,25 +61,33 @@ def find_mh_params():
     results = []
     for v in np.geomspace(0.001, 0.3, 30):
         logging.critical(f"Measuring time to target ess with {v=}")
-        results.append((v, ess_over_time((v,), metropolis_hastings_iterator)))
+        t = ess_over_time((v,), metropolis_hastings_iterator)
+        if t is not None:
+            results.append((v, t))
+            logging.info(results)
 
-        print(results)
+    logging.info(results)
 
 
 def find_hmc_params():
     results = []
-    for m, e, L in itertools.product((0.5, 1.0, 2.0),
-                                     np.linspace(0.1, 0.5, 4),
-                                     (3, 5)):
+    for m, e, L in itertools.product(np.linspace(0.5, 2.0, 4),
+                                     np.geomspace(0.001, 0.2, 6),
+                                     (3, 5, 7, 10)):
         logging.critical(f"Measuring time to target ess with {m=}, {e=}, {L=}")
         M = np.eye(2) * m
-        results.append(((m, e, L), ess_over_time((M, e, L), hamilton_monte_carlo_iterator)))
+        t = ess_over_time((M, e, L), hamilton_monte_carlo_iterator)
+        if t is not None:
+            results.append(((m, e, L), t))
+            logging.info(results)
 
-        print(results)
+    logging.info(results)
 
 
 def main():
-    logging.basicConfig(level=logging.CRITICAL, format="[%(asctime)s %(levelname)s] %(message)s")
+    logging.basicConfig(level=logging.WARNING, filename="hparams_grid_search.log", filemode="w",
+                        format="[%(asctime)s %(levelname)s] %(message)s")
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
     # find_mh_params()
     find_hmc_params()
 
